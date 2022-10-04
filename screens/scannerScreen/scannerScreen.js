829a09ea459bb4d4ae085eeb602a430387b593e9
Andrey <smokels1998@gmail.com>
2022-10-04 21:47:55 +0300

    barscanner is added

--- screens/scannerScreen/scannerScreen.js
+++ screens/scannerScreen/scannerScreen.js
@@ -1,12 +1,84 @@
 import * as React from 'react';
-import { View, Text} from 'react-native';
+import { useState, useEffect } from 'react';
+import { StyleSheet, View, Text, Button } from 'react-native';
+import { BarCodeScanner, barCodeScanner } from 'expo-barcode-scanner';
 
 function ScannerScreen() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+  const [text, setText] = useState('Not yet scanned');
+
+  const askForCameraPermission = () => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status == 'granted');
+    })();
+  };
+
+  useEffect(() => {
+    askForCameraPermission();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    setText(data);
+    console.log(`type: ${type};\ndata: ${data}`);
+  };
+
+  if (hasPermission === null) {
+    return (
+      <View>
+        <Text>Requesting for camera permession</Text>
+      </View>
+    );
+  }
+
+  if (hasPermission === false) {
+    return (
+      <View>
+        <Text>No access to camera</Text>
+        <Button title={'Allow camera'} onPress={() => askForCameraPermission()}></Button>
+      </View>
+    );
+  }
+
   return (
-    <View>
-        <Text>ScannerScreen</Text>
+    <View style={styles.container}>
+      <View style={styles.barcodebox}>
+        <BarCodeScanner
+          style={{ height: 400, width: 400 }}
+          onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        />
+      </View>
+      <Text style={styles.maintext}>{text}</Text>
+
+      {scanned && <Button title={'scan again?'} onPress={() => setScanned(false)} color="tomato" />}
     </View>
   );
 }
 
-export default ScannerScreen; 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  barcodebox: {
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 300,
+    width: 300,
+    overflow: 'hidden',
+    borderRadius: 30,
+    backgroundColor: 'tomato',
+  },
+
+  maintext: {
+    fontSize: 16,
+    margin: 20,
+  },
+});
+
+export default ScannerScreen;
